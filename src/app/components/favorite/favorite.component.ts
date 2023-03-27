fc8da89cb70bfdf11938d837c5bdc2830f1bb5ec
Niko <soder.n@gmail.com>
2023-03-27 12:26:33 +0300

    Test Helsinki open api in favorite component

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent {
+  public activities_api_url =
+    'https://cors-anywhere.herokuapp.com/https://open-api.myhelsinki.fi/v1/activities/';
 
-  randomPhoto = "https://source.unsplash.com/random/?restaurant/300x100"
-  isVisable: boolean = false
+  randomPhoto = 'https://source.unsplash.com/random/?restaurant/300x100';
+  isVisable: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get(this.activities_api_url).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
