ab1b85e45b96a5b9d266019e17dc2be5e7924a57
Niko <soder.n@gmail.com>
2023-03-27 12:25:15 +0300

    Add favorite path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { MapComponent } from './components/map/map.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'map', component: MapComponent},
-  
+  { path: 'map', component: MapComponent },
+  { path: 'favorite', component: FavoriteComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
