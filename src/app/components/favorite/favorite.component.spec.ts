39f7ab988d3d4df3c4d9f42b8dfe5b265b43a350
Niko <soder.n@gmail.com>
2023-03-28 11:12:46 +0300

    Add tests to removeFavorite method and ng-template

--- src/app/components/favorite/favorite.component.spec.ts
+++ src/app/components/favorite/favorite.component.spec.ts
@@ -18,4 +18,39 @@ describe('FavoriteComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should remove item from favorites array', () => {
+    component.favorites = [
+      {
+        img: 'test image',
+        title: 'Title 1',
+        description: 'Description about place here',
+        address: 'Address 1',
+      },
+      {
+        img: 'test image',
+        title: 'Title 2',
+        description: 'Description about place here',
+        address: 'Address 2',
+      },
+    ];
+    expect(component.favorites.length)
+      .withContext('length should be 2')
+      .toBe(2);
+    component.removeFavorite(component.favorites[0].title); // remove first object
+    fixture.detectChanges();
+    expect(component.favorites.length)
+      .withContext('length should be 1')
+      .toBe(1);
+    expect(component.favorites[0].title).toBe('Title 2');
+  });
+
+  it('should display ng-template if no favorites', () => {
+    component.favorites = [];
+    fixture.detectChanges();
+    let divEl: HTMLElement =
+      fixture.nativeElement.querySelector('.no-favorites');
+
+    expect(divEl.textContent).toBe('No favorites found');
+  });
 });
