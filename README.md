1dc2aff63f64db6b98ca134cecea11fa93a4f3d3
elis1386 <elis1386@gmail.com>
2023-03-29 13:55:38 +0300

    auth user

--- README.md
+++ README.md
@@ -27,8 +27,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 # NearMe
 
-# Google map apiKey
-AIzaSyAp2aFUhqhey1y77f7FBb-7L6KraRxdC1M
 
 
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  collectionData,
+  addDoc,
+  Firestore,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  docData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   imgUrl = 'assets/logoNM2.png';
+  randomPhoto = 'https://source.unsplash.com/random/?restaurant/300x100';
+  isLogged: boolean = false;
+  user: any;
+  currentUser: any;
+  constructor(
+    public authService: AuthService,
+    public auth: AngularFireAuth,
+    public database: AngularFirestore,
+    private zone: NgZone
+  ) {}
+
+  ngOnInit(): void {
+    this.zone.run(() => {
+      let loggedUser = JSON.parse(localStorage.getItem('user')!);
+      this.currentUser = loggedUser;
+    });
+  }
 }
