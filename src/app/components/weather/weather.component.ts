bea956bae35cd304d080248a7f7e371e149c8e6f
Niko <soder.n@gmail.com>
2023-03-31 12:29:29 +0300

    Add weather response types

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { env } from 'src/config';
 import { HttpClient } from '@angular/common/http';
+import { Weather_res, Weather_info } from 'src/app/models/weather';
 
 @Component({
   selector: 'app-weather',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent {
-  weatherInfo: any = {};
+  weatherInfo: Partial<Weather_info> = {};
   WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=60.18608603539844&lon=24.943128762153208&appid=${env.WEATHER_API_KEY}&units=metric`;
 
   constructor(private http: HttpClient) {}
@@ -18,7 +19,7 @@ export class WeatherComponent {
   }
 
   getWeather() {
-    this.http.get(this.WEATHER_API_URL).subscribe((res: any) => {
+    this.http.get<Weather_res>(this.WEATHER_API_URL).subscribe((res) => {
       const city = res.city.name;
       const weatherArray = res.list.slice(0, 10);
       // clean up some results
@@ -26,8 +27,6 @@ export class WeatherComponent {
         const element = weatherArray[i];
         element.main.temp = Math.round(element.main.temp);
         element.weather[0].icon = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
-        /* const times = element.dt_txt.split(' ');
-        element.dt_txt = times[1].slice(0, 5); */
         element.dt_txt = element.dt_txt.slice(5, 16);
       }
       this.weatherInfo = {
