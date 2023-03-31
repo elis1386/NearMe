f747461ed3a9c4d131352bc41e2824e40ad28064
Niko <soder.n@gmail.com>
2023-03-31 09:50:53 +0300

    Generate weather component

---
+++ src/app/components/weather/weather.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { env } from 'src/config';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css'],
+})
+export class WeatherComponent {
+  weatherInfo: any = {};
+  WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=60.18608603539844&lon=24.943128762153208&appid=${env.WEATHER_API_KEY}&units=metric`;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.http.get(this.WEATHER_API_URL).subscribe((res: any) => {
+      const city = res.city.name;
+      const weatherArray = res.list.slice(0, 4);
+      // clean up some results
+      for (let i = 0; i < weatherArray.length; i++) {
+        const element = weatherArray[i];
+        element.main.temp = Math.round(element.main.temp);
+        element.weather[0].icon = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
+        const times = element.dt_txt.split(' ');
+        element.dt_txt = times[1];
+      }
+
+      this.weatherInfo = {
+        city: city,
+        weatherArr: weatherArray,
+      };
+    });
+  }
+}
