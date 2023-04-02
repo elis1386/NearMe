32827ae3f42337f4e68296c28918d1225f53b396
Niko <soder.n@gmail.com>
2023-04-02 16:33:27 +0300

    Add prettierrc

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-favorite',
-  templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  selector: "app-favorite",
+  templateUrl: "./favorite.component.html",
+  styleUrls: ["./favorite.component.css"],
 })
 export class FavoriteComponent {
-
-  randomPhoto = "https://source.unsplash.com/random/?restaurant/300x100"
-  isVisable: boolean = false
+  randomPhoto = "https://source.unsplash.com/random/?restaurant/300x100";
+  isVisable: boolean = false;
 }
