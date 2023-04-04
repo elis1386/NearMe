7729a26797042f4f9133960be7f3cc50b6b8368d
elis1386 <elis1386@gmail.com>
2023-04-04 12:14:19 +0300

    add map with places api

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 
 @Component({
@@ -7,14 +8,10 @@ import { Component } from "@angular/core";
 })
 export class MapComponent {
   isVisible: boolean = false;
-  apiKye: string = "AIzaSyAp2aFUhqhey1y77f7FBb-7L6KraRxdC1M";
+  apiKye: string = "AIzaSyALxmPNw9nwGV23i6CAf_pcMdYG8-unr28";
 
   /* Map */
 
-  /*   map = new google.maps.Map((),{
-    center: { lat: 60.18608603539844, lng: 24.943128762153208 },
-    zoom: 8,
-  }); */
   display: any;
   center: google.maps.LatLngLiteral = {
     lat: 60.18608603539844,
@@ -22,3 +19,7 @@ export class MapComponent {
   };
   zoom = 13;
 }
+
+
+/* API_KEY for all requests 
+https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=restaurant&name=harbour&key= AIzaSyALxmPNw9nwGV23i6CAf_pcMdYG8-unr28*/
