72f4d8f29f783b6d39e1ba7b367c1d5ba421a437
elis1386 <elis1386@gmail.com>
2023-04-11 09:57:33 +0300

    add laibrarias to the index script link

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -11,33 +11,26 @@ import {} from "@angular/google-maps";
 })
 export class MapComponent implements OnInit {
   isVisible: boolean = false;
-  PLACES_API_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500ey=
-    ${mapKey.MAP_API_KEY}`;
-  showMapPill!: boolean;
   mapLoaded!: boolean;
   map!: google.maps.Map;
+  geocoder = new google.maps.Geocoder();
 
-  center: google.maps.LatLngLiteral = {
-    lat: 60.18608603539844,
-    lng: 24.943128762153208,
-  };
-  zoom!: 10;
   options: google.maps.MapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     scrollwheel: true,
     disableDefaultUI: true,
     disableDoubleClickZoom: true,
+    center: {
+      lat: 60.18608603539844,
+      lng: 24.943128762153208,
+    },
+    zoom: 15,
   };
 
-
-
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone, httpClient: HttpClient) {}
 
   ngOnInit() {
-      // initialize the map 
-      this.map = new google.maps.Map(document.getElementById("#map")!, {
-        ...this.options,
-      });
-      console.log(this.options);
+    // initialize the map
+    this.map = new google.maps.Map(document.getElementById("#map")!, {...this.options});
   }
 }
