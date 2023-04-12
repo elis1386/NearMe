f1a1576c890536a00d6000275b4e5f7bf2f6cc16
Niko <soder.n@gmail.com>
2023-04-12 12:34:05 +0300

    Pass places data to list component and show it on the sidebar

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent {
-
+  @Input() placesList: any[] = [];
 }
