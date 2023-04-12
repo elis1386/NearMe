e5ca8f6888223c0cbdbe0a564a2548b119270459
Niko <soder.n@gmail.com>
2023-04-12 15:02:28 +0300

    Add stars for places

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "app-list",
@@ -7,4 +8,22 @@ import { Component, Input } from "@angular/core";
 })
 export class ListComponent {
   @Input() placesList: any[] = [];
+
+  // add ratingArray value to objects so we can loop trough and add stars
+  // does this everytime placesList changes
+  ngOnChanges(changes: SimpleChanges) {
+    for (let i = 0; i < changes["placesList"].currentValue.length; i++) {
+      let element = changes["placesList"].currentValue[i];
+
+      if (element.rating) {
+        let emptyArr = [];
+        let roundedRating = Math.round(element.rating);
+        for (let j = 0; j < roundedRating; j++) {
+          const rating = j;
+          emptyArr.push(rating);
+        }
+        element.ratingArray = emptyArr;
+      }
+    }
+  }
 }
