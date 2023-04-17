78ada00d0c9289266ee6bd2f3f978e3a11ca9b98
Niko <soder.n@gmail.com>
2023-04-17 12:41:05 +0300

    Move sidebar logic to own component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"],
 })
 export class SidebarComponent {
-
+  @Input() placesList: any[] = [];
 }
