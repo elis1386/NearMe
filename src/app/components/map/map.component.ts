845d45218e4886202839e31ab975e383f94da4ca
elis1386 <elis1386@gmail.com>
2023-04-18 15:26:01 +0300

    add correct type of searching places

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -47,7 +47,7 @@ export class MapComponent implements OnInit {
     this.reset();
     const searchRequest: google.maps.places.PlaceSearchRequest = {
       type: type,
-      radius: 1500,
+      radius: 1000,
       location: this.options.center!,
     };
     this.service.nearbySearch(searchRequest, (results, status) => {
