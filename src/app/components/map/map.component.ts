c6755a6f1096a1be6e4752d16e5a9639938ff90d
elis1386 <elis1386@gmail.com>
2023-04-28 19:44:49 +0300

    changed types of places and changed filters

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -134,10 +134,14 @@ export class MapComponent implements OnInit {
     });
   }
 
-  addToFavorite(place: Favorite) {
+  addToFavorite(place: Favorite, ) {
     console.log(place);
     this.userId = JSON.parse(localStorage.getItem("user")!).uid;
     place.userId = this.userId;
     this.requestService.addToFavorite(place);
+    setTimeout(()=>{
+      this.infoWindow.close();
+    },300)
+    
   }
 }
