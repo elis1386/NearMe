9e3a5c513cb26696b406370833dfded2f26925a7
elis1386 <elis1386@gmail.com>
2025-04-28 14:28:34 +0300

    to v16

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,19 +1,13 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     public authService: AuthService,
     public router: Router,
